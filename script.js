4ee955895ce3a0cac10ac132a0367ec0bd38a1af
perugi <dominik.perusko@gmail.com>
2023-10-15 22:39:20 +0200

    Return a blank board from the createBlankBoard, instead of operating on the existing one

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const gameboard = (() => {
-  let board = [];
-  _createBlankBoard();
+  let board = _createBlankBoard();
 
   function _createBlankBoard() {
+    let board = [];
     for (let i = 0; i < 3; i++) {
       board[i] = [];
       for (let j = 0; j < 3; j++) {
         board[i].push(" ");
       }
     }
-    console.table(board);
+
+    return board;
   }
 
   function getBoard() {
